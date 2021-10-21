d23229cf2785241a3cc686eda9fb521c3170a196
iliass <mohammediliassaffani@gmail.com>
2021-10-21 13:19:16 +0100

    commit

--- welcome.php
+++ welcome.php
@@ -15,11 +15,12 @@ if (!isset($_SESSION['username'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title class="wlcm">Welcome</title>
 </head>
 <body>
     
-    <?php echo "<h1>Welcome " . $_SESSION['username'] . "</h1>"; ?>
-    <a class="logout" href="logout.php">Logout</a>
+    <?php echo "<h1 class= 'text-white'>Welcome " . $_SESSION['username'] . "</h1>"; ?>
+    <a class='btn btn-success' href="logout.php">Logout</a>
 </body>
 </html>
